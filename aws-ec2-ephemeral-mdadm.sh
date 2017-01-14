95009c9bd601db562b5d613eee7332980ff2cfc6
Alexander Kyxap <kyxap@pro-manage.net>
2017-01-14 17:34:59 +0300

    Added xfs/ext4 labels. Added single drive support.

--- aws-ec2-ephemeral-mdadm.sh
+++ aws-ec2-ephemeral-mdadm.sh
@@ -1,14 +1,25 @@
 #!/usr/bin/env bash
 set -e
-DEVICES=(${DEVICES[@]:-/dev/xvdb /dev/xvdc}) \
-  MOUNTPOINT="${MOUNTPOINT:-/var/lib/cassandra/data}" \
-  CONF="${CONF:-/etc/mdadm/mdadm.conf}" \
-  MD="${MD:-/dev/md127}" \
-  FS="${FS:-xfs}"
+# Usage:
+#   DEVICES=(/dev/xvdf /dev/xvdg) MOUNTPOINT="/var/lib/postgresql" \
+#   MOUNTOPTS="relatime,nofail" CONF="/etc/mdadm/mdadm.conf" LABEL="PGDATA" \
+#   MD="/dev/md127" FS="xfs" RAID="ebs" /path/to/mdadm.sh
+# Opts:
+#   FS=<xfs|ext4>
+#   RAID=<ebs|ephemeral|noraid>
+
+DEVICES=(${DEVICES[@]:-/dev/xvdb /dev/xvdc})
+MOUNTPOINT="${MOUNTPOINT:-/var/lib/cassandra/data}"
+MOUNTOPTS="${MOUNTOPTS:-relatime,nofail}"
+CONF="${CONF:-/etc/mdadm/mdadm.conf}"
+LABEL="${LABEL:-CASSDATA}"
+MD="${MD:-/dev/md127}"
+FS="${FS:-xfs}"
+RAID="${RAID:-ephemeral}"
 
 function prepareenv  { mkdir -p "${CONF%/*}"; mkdir -p "${MOUNTPOINT}"; }
 function createconf  { echo "DEVICE ${DEVICES[@]}" | tee ${CONF}; mdadm --detail --scan | tee -a ${CONF}; }
-function createfs    { mkfs.${FS} -f ${MD} -L ${HOSTNAME%%\.*}; }
+function createfs    { mkfs.${FS} -L ${LABEL} -f ${MD}; }
 function mountfs     { mountpoint -q ${MOUNTPOINT} || mount ${MD} ${MOUNTPOINT}; }
 function umountfs    { mountpoint -q ${MOUNTPOINT} || return 0 && umount ${MOUNTPOINT}; }
 function createmd    { echo y | mdadm --create ${MD} --level=0 --raid-devices=${#DEVICES[@]} ${DEVICES[@]}; }
@@ -20,8 +31,13 @@ function destroyconf { [[ -s ${CONF} ]] || return 0 && rm -rf "${CONF%/*}"; }
 function lookupdev   { printf "%s\n" "${DEVICES[@]##*/}" | grep -cwof - /proc/partitions; }
 function statusdev   { [[ -s ${CONF} ]] && { [[ -b ${MD} ]] || return 0; }; exammd; }
 function validatedev { if n=$( lookupdev ); then [[ ${n} -eq ${#DEVICES[@]} ]] || exit 1; fi; }
+function addfstab    { echo "LABEL=${LABEL} ${MOUNTPOINT} ${FS} ${MOUNTOPTS} 0 0" | tee -a /etc/fstab >/dev/null; }
+function delfstab    { grep -v "LABEL=${LABEL} ${MOUNTPOINT} ${FS} ${MOUNTOPTS} 0 0" /etc/fstab > /tmp/fstab; mv -f /tmp/fstab /etc/fstab; }
+function createlabel { [[ ${FS} == ext[234] ]] && tune2fs -L ${LABEL} ${MD}; [[ ${FS} == xfs ]] && xfs_admin -L ${LABEL} ${MD}; }
+function finish      { exit 0; }
 
 CMD=${@:-create}
-[[ ${CMD} == status ]]  && { statusdev; }
-[[ ${CMD} == create ]]  && { validatedev; prepareenv; createmd; createfs; createconf; mountfs; }
-[[ ${CMD} == destroy ]] && { umountfs; destroyconf; stopmd; destroymd; }
+[[ ${RAID} == noraid  ]] && { validatedev; prepareenv; createlabel; addfstab; mountfs && finish || finish; }
+[[ ${CMD}  == status  ]] && { statusdev && finish; }
+[[ ${CMD}  == create  ]] && { validatedev; prepareenv; createmd; createfs; createlabel; createconf; addfstab; mountfs && finish; }
+[[ ${CMD}  == destroy ]] && { umountfs; delfstab; destroyconf; stopmd; destroymd && finish; }
